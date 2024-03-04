847dae5cb131570711f2732e2dd6311aa753e627
Orin Chen <orinchen@gmail.com>
2024-03-04 10:49:02 +0800

    feat: 添加时间校验

---
+++ xvalidator/datetime.go
@@ -0,0 +1,25 @@
+package xvalidator
+
+import (
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	"github.com/orinchen/xlib/xtime"
+)
+
+var DatetimeVerifyTag = "xv_datetime"
+var DatetimeErrorInfo = "{0}:电话号码格式不正确"
+
+func RegisterDatetimeValidation(validate *validator.Validate, trans ut.Translator) error {
+	err := validate.RegisterValidation(CnPhoneNumberVerifyTag, VerifyDatetime)
+	if trans == nil || err != nil {
+		return err
+	}
+
+	return validate.RegisterTranslation(DatetimeVerifyTag, trans, registerTranslator(DatetimeVerifyTag, DatetimeErrorInfo), translate)
+}
+
+func VerifyDatetime(f validator.FieldLevel) bool {
+	val := f.Field().String()
+	_, err := xtime.AutoParse(val)
+	return err == nil
+}
